8848cc724f5718554a79af94807d82af73b888d1
gabrielres10 <gabrielrestrepo7@gmail.com>
2024-04-23 14:21:17 -0500

    seed: Add admin user to users seed

--- src/seed/data/users.seed.ts
+++ src/seed/data/users.seed.ts
@@ -3,24 +3,31 @@ import * as bcryptjs from 'bcryptjs';
 
 export const usersSeed: CreateUserDto[] = [
     {
-        id: "111111",
-        name: "John Doe",
-        email: "john@example.com",
-        password: bcryptjs.hashSync("12345678", 10),
-        role: ["user"]
-      },
-      {
-        id: "222222",
-        name: "Jane Smith",
-        email: "jane@example.com",
-        password: bcryptjs.hashSync("12345678", 10),
-        role: ["technician"]
-      },
-      {
-        id: "333333",
-        name: "Mike Morgan",
-        email: "mike@example.com",
-        password: bcryptjs.hashSync("12345678", 10),
-        role: ["technician"]
-      }
+      id: "123456",
+      name: "Administrator",
+      email: "admin@example.com",
+      password: bcryptjs.hashSync("123456", 10),
+      role: ["super-user"]
+    },
+    {
+      id: "111111",
+      name: "John Doe",
+      email: "john@example.com",
+      password: bcryptjs.hashSync("123456", 10),
+      role: ["user"]
+    },
+    {
+      id: "222222",
+      name: "Jane Smith",
+      email: "jane@example.com",
+      password: bcryptjs.hashSync("123456", 10),
+      role: ["technician"]
+    },
+    {
+      id: "333333",
+      name: "Mike Morgan",
+      email: "mike@example.com",
+      password: bcryptjs.hashSync("123456", 10),
+      role: ["technician"]
+    }
 ];
