9e15db5e6218cd070e79a7182bf4bf54a6bb3120
gabrielres10 <gabrielrestrepo7@gmail.com>
2024-04-23 14:43:34 -0500

    security: Add protection to seed get method

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,11 +1,18 @@
 import {Controller, Get} from '@nestjs/common';
 import { SeedService } from './seed.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RoleProtected } from 'src/auth/decorators/role-protected.decorator';
+import { UserRoleGuard } from 'src/auth/guard/user-role.guard';
+import { UseGuards } from '@nestjs/common';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('seed')
 export class SeedController {
     constructor(private readonly seedService: SeedService) {}
 
     @Get()
+    @UseGuards(AuthGuard, UserRoleGuard)
+    @RoleProtected(ValidRoles.technician, ValidRoles.superUser)
     runSeed(){
         return this.seedService.populateDB();
     }
