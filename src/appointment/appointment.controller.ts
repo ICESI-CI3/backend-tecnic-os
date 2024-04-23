2e138d68bbf65d728294ce6351983beabfd494bf
gabrielres10 <gabrielrestrepo7@gmail.com>
2024-04-22 19:07:35 -0500

    auth: add authorization to appointment routes

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -13,28 +13,37 @@ export class AppointmentController {
 
   @Post('create')
   @UseGuards(AuthGuard, UserRoleGuard)
-  @RoleProtected(ValidRoles.technician)
+  @RoleProtected(ValidRoles.technician, ValidRoles.superUser)
   create(@Body() createAppointmentDto: CreateAppointmentDto, @Request() req) {
     const technicianId= req.user.id;
     return this.appointmentService.create(createAppointmentDto, technicianId);
   }
 
+  
   @Get()
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.technician, ValidRoles.superUser)
   findAll() {
     return this.appointmentService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.technician, ValidRoles.superUser)
   findOne(@Param('id') id: string) {
     return this.appointmentService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.technician, ValidRoles.superUser)
   update(@Param('id') id: string, @Body() updateAppointmentDto: UpdateAppointmentDto) {
     return this.appointmentService.update(id, updateAppointmentDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard, UserRoleGuard)
+  @RoleProtected(ValidRoles.technician, ValidRoles.superUser)
   remove(@Param('id') id: string) {
     return this.appointmentService.remove(id);
   }
