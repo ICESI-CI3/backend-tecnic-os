6167e53443cba2d3fadaa0779734460bf9b442f8
gabrielres10 <gabrielrestrepo7@gmail.com>
2024-06-04 01:11:24 -0500

    Minor changes

--- src/appointment/dto/update-appointment.dto.ts
+++ src/appointment/dto/update-appointment.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString } from "class-validator";
 import { CreateAppointmentDto } from './create-appointment.dto';
 
-export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {}
+export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {
+    @IsString()
+    description: string;
+}
--- src/seed/data/appointment.seed.ts
+++ src/seed/data/appointment.seed.ts
@@ -1,26 +1,26 @@
 import { CreateAppointmentDto } from "src/appointment/dto/create-appointment.dto";
 
 interface AppointmentData{
-    technicianId: string;
+    userId: string;
     appointmentDTO: CreateAppointmentDto;
 }
 
 
 export const appointmentsSeed: AppointmentData[] = [
     {
-        technicianId: "222222",
+        userId: "222222",
         appointmentDTO: {
             description: "Cita con técnico plumber",
-            userId: "111111",
+            technicianId: "111111",
             date: "2024-04-24",
             initTime: "09:00"
         }
     },
     {
-        technicianId: "333333",
+        userId: "333333",
         appointmentDTO: {
             description: "Cita con técnico plumber, builder y electrician",
-            userId: "111111",
+            technicianId: "111111",
             date: "2024-04-25",
             initTime: "10:00"
         }
